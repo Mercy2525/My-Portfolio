e3ac01db1264be2ef36c85ce71179d2f2804d9b6
ASUS <you@example.com>
2023-09-06 09:45:54 +0300

    created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Experience from './Pages/Experience';
+import Projects from './Pages/Projects';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     
+    <Router>
+     <Navbar/>
+       <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/project' element={<Projects/>}/>
+          <Route path='/experience' element={<Experience/>}/>
+       </Routes> 
+    </Router>
+  
     </div>
   );
 }
