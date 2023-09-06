d4a041d0b8b78c1da27d4097942345215985f499
Mercy_Muriithi <mercywmuriithi.mma@gmail.com>
2023-09-06 11:52:54 +0300

    navbar details and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Home from './Pages/Home';
@@ -13,7 +12,7 @@ function App() {
      <Navbar/>
        <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/project' element={<Projects/>}/>
+          <Route path='/projects' element={<Projects/>}/>
           <Route path='/experience' element={<Experience/>}/>
        </Routes> 
     </Router>
