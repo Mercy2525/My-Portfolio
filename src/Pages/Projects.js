9ddfd8a91da6e8981877ff5d0988ec9a9754fded
Mercy_Muriithi <mercywmuriithi.mma@gmail.com>
2023-09-07 08:45:59 +0300

    project and project item defined

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import ProjectItem from '../Components/ProjectItem'
+import "../Styles/Project.css"
+import proj1 from "../assets/proj1.jpg"
+import movie from "../assets/movie.jpg"
+import shopIG from "../assets/shopIG.jpg"
 
 function Projects() {
   return (
-    <div>
-    <h1>Projects</h1>
+    <div className='projects'>
+    <h1>My Personal Projects</h1>
+    <div className='projectList'>
+      <ProjectItem name="Social Media Website" image={proj1}/>
+      <ProjectItem name="Ticket-Booking Website" image={movie} />
+      <ProjectItem name="E-commerce Website" image={shopIG}/>
+    </div>
     </div>
   )
 }
