9ee7afb2f90f433bd0d4d6c356c13323bd8b365b
Mercy <mercywmuriithi.mm@gmail.com>
2023-10-09 11:34:29 +0300

    Revert "project and project item defined"
    This reverts commit 9ddfd8a91da6e8981877ff5d0988ec9a9754fded.

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import ProjectItem from '../Components/ProjectItem'
-import "../Styles/Project.css"
-import proj1 from "../assets/proj1.jpg"
-import movie from "../assets/movie.jpg"
-import shopIG from "../assets/shopIG.jpg"
 
 function Projects() {
   return (
-    <div className='projects'>
-    <h1>My Personal Projects</h1>
-    <div className='projectList'>
-      <ProjectItem name="Social Media Website" image={proj1}/>
-      <ProjectItem name="Ticket-Booking Website" image={movie} />
-      <ProjectItem name="E-commerce Website" image={shopIG}/>
-    </div>
+    <div>
+    <h1>Projects</h1>
     </div>
   )
 }
